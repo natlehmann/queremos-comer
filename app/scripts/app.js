84689750078396876ea2d421094308c5fc33f31c
natlehmann <natlehmann@gmail.com>
2016-05-25 09:49:52 -0300

    se arma primer mapa de recetas en memoria

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,7 +22,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       StatusBar.styleDefault();
     }
   });
-})
+});
 
 
 
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -2,12 +2,15 @@
 
 angular.module('starter.services', ['ionic'])
 
-.constant('MINIMO_RECETAS_RECOMENDADAS', 3)
+.constant('CARNES_LACTEOS_HUEVOS', 'carnes')
+.constant('VEGETALES', 'vegetales')
+.constant('PASTAS_CEREALES_LEGUMBRES', 'cereales')
+.constant('TIPO_A', 'A')
+.constant('TIPO_B', 'A')
 
-.factory('Carta', function ($http) {
+.factory('Carta', function ($http, CARNES_LACTEOS_HUEVOS, VEGETALES, PASTAS_CEREALES_LEGUMBRES, TIPO_A) {
 	
 	function getCarta() {
-
     	return $http.get('carta.json');    
     }
 
@@ -15,8 +18,44 @@ angular.module('starter.services', ['ionic'])
     
     getRecetaDeHoy: function(callback) {
     	
+    	var primerosPlatos = {
+    			CARNES_LACTEOS_HUEVOS: [],
+    			VEGETALES: [],
+    			PASTAS_CEREALES_LEGUMBRES: []
+    	};
+    	var guarniciones = {
+    			TIPO_A: [],
+    			TIPO_B: []
+    	};
+    	
     	getCarta().success(function (response) {
-    		callback(response.primerPlato[0].nombre + response.guarnicion[0].nombre);
+    		
+    		response.primerPlato.forEach(function(item){
+    			
+    			if (item.categoria === CARNES_LACTEOS_HUEVOS) {
+    				primerosPlatos.CARNES_LACTEOS_HUEVOS.push(item);
+    				
+    			} else {
+    				if (item.categoria === VEGETALES) {
+    					primerosPlatos.VEGETALES.push(item);
+    					
+    				} else {
+    					primerosPlatos.PASTAS_CEREALES_LEGUMBRES.push(item);
+    				}
+    			}
+    		});
+    		
+    		response.guarnicion.forEach(function(item){
+    			
+    			if (item.tipo === TIPO_A) {
+    				guarniciones.TIPO_A.push(item);
+    				
+    			} else {
+    				guarniciones.TIPO_B.push(item);
+    			}
+    		});
+    		
+    		callback(primerosPlatos.VEGETALES[0].nombre + " con " + guarniciones.TIPO_A[0].nombre);
     	});
     }
   };
